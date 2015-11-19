2d59310853610d0364e5295bccfaf3794c89b73a
Karla Magallanes <karlaimagallanes@gmail.com>
2015-11-18 20:06:51 -0800

    Initial Commit
    new project

---
+++ MidEvaluation/MindUniversity.MidEvaluation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MindUniversity.MidEvaluation.MvcApplication" Language="C#" %>
---
+++ MidEvaluation/MindUniversity.MidEvaluation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MindUniversity.MidEvaluation.Startup))]
+namespace MindUniversity.MidEvaluation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
